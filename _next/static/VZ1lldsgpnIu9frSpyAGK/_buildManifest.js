a6d668376d68ca7ae6ce6a07751063d6519beeaa
“David <Dave@cyberlark.com.au>
2022-10-16 22:47:58 +1000

    Updates

--- _next/static/VZ1lldsgpnIu9frSpyAGK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/249-60e6759983c4632a.js","static/chunks/pages/index-84028a7018a12724.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
